cd8f4bb6aede0a1de657c369ab20749e54020a4e
lihongxun945 <lihongxun945@gmail.com>
2016-08-11 10:46:44 +0800

    add result page

--- src/components/result/index.vue
+++ src/components/result/index.vue
@@ -1,17 +1,44 @@
 <template>
-  <div class="result">
-    <h1>充值成功</h1>
-    <p>我们正在处理您的充值，请稍后查看账户余额</p>
-
-    <div class="buttons">
-      <a class="button">查看余额</a>
-      <a class="button button-light">返回</a>
+  <div class="result result-{{type}}">
+    <div class="brand-icon">
+      <slot name="icon">
+        <div class="icon icon-{{type}}-fill"></div>
+      </slot>
     </div>
 
-    <div class="bottom">充值协议</div>
+    <h1><slot name="title">SUCCESS!</slot></h1>
+    <div class="text">
+      <slot name="text">
+        <p>Some more message</p>
+      </slot>
+    </div>
+
+
+    <div class="buttons">
+      <slot name="buttons">
+        <a class="button">OK</a>
+        <a class="button button-light">BACK</a>
+      </slot>
+    </div>
+
+    <div class="bottom">
+      <slot name="bottom"></slot>
+    </div>
   </div>
 </template>
 
-<style lang="less" scoped>
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: 'success'  // success, error, forbidden
+    }
+  }
+}
+</script>
+
+<style lang="less">
+// not scoped to make style apply on slot elements
 @import './result.less';
 </style>
--- src/demos/Result.vue
+++ src/demos/Result.vue
@@ -2,7 +2,19 @@
   <div class="page">
     <simple-header title="Result" :back-link="true"></simple-header>
     <content>
-      <result></result>
+      <result type="success">
+        <div slot="title">Commit Success</div>
+        <div slot="text">
+          Your commit have been recieved!
+        </div>
+        <div slot="buttons">
+          <a class="button" href="javascript:;">OK</a>
+          <a class="button button-light" href="javascript:;">BACK</a>
+        </div>
+        <div slot="bottom">
+          <a>About This Page</a>
+        </div>
+      </result>
     </content>
   </div>
 </template>
