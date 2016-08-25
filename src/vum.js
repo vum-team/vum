4d3a01ef1b513895204ede3036521267c5334ec2
lihongxun945 <lihongxun945@gmail.com>
2016-08-25 11:15:41 +0800

    more intelligent router

--- src/vum.js
+++ src/vum.js
@@ -17,7 +17,7 @@ class RouterConfig {
     this.router.beforeEach(function (t) {
       const to = t.to.path
       const from = t.from.path
-      if (history.getItem(to)) {
+      if (history.getItem(to) || from.startsWith(to)) {
         router.app.$el.className = 'transition-reverse'
         history.removeItem(to)
       } else {
